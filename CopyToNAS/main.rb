a2a7c075af3505460d00937fe53de4b5ae73c3ed
Madsn <m@madsn.net>
2012-01-15 21:05:08 +0100

    fixed issue with regex where series starting with s would be recognized incorrectly. Prompts user to approve all copies

--- CopyToNAS/main.rb
+++ CopyToNAS/main.rb
@@ -14,25 +14,43 @@ puts shows
 
 pattern = /S\d{1,2}E\d{1,2}/
 not_pattern = /[^(S\d{1,2})&&[^(E\d{1,2})]]+/
+commands = []
 
 # Loop through directories/files in local dir
 Dir.entries(local_dir).each() { |file|
   #puts file
-  if file.index(pattern) != nil
-    puts file
-    # The name matches a possible tv show, check if the show is in the shows array
-    # First get the name of the series from filename
-    filename = file[not_pattern].strip
-    puts filename
-    if shows.has_key?(filename)
-      # Copy file to remote dir
-      mkdircmd = ("mkdir "+ "\""+shows[filename]+"/"+file+"/\"").gsub("/","\\")
-      copycmd = ("copy \""+local_dir+file+"\" \""+shows[filename]+"/"+file+"/\"").gsub("/","\\")
-      puts mkdircmd
-      puts copycmd
-      system(mkdircmd)
-      system(copycmd)
-    end
-    #puts "blap"
-  end
+	if file.index(pattern) != nil
+		#puts file
+		# The name matches a possible tv show, check if the show is in the shows array
+		# First get the name of the series from filename
+		filename = file.slice(0, file.index(pattern)).strip
+		#puts filename
+		if shows.has_key?(filename)
+		    # Copy file to remote dir
+			mkdircmd = ("mkdir \""+shows[filename]+"/"+file+"/\"").gsub("/","\\")
+			copycmd = ("copy \""+local_dir+file+"\" \""+shows[filename]+"/"+file+"/\"").gsub("/","\\")
+			delcmd = ("rmdir \""+local_dir+file+"\" /S /Q")
+			puts mkdircmd
+			puts copycmd
+			puts delcmd
+			puts "Add above commands to queue? Enter to approve, \"s\" to skip, \"k\" to keep (no delete)"
+			input = gets.strip
+			if input == "s"
+				
+			elsif input == "k"
+				commands += [mkdircmd, copycmd]
+			else
+				commands += [mkdircmd, copycmd, delcmd]
+			end
+		    #system(mkdircmd)
+		    #system(copycmd)
+		end
+		#puts "blap"
+	end
+}
+
+print "------------------------------------"
+
+commands.each() { |cmd|
+	system(cmd)
 }
